9349c767f46b76cae5b50943fae6eae0988204b4
seiya-miura <s.miura1811@morijyobi.ac.jp>
2019-06-04 11:12:54 +0900

    [modify]Conflict.java

--- Conflict.java
+++ Conflict.java
@@ -1,5 +1,7 @@
-public class Hello{
+public class For{
 	public static void main(String[] args){
-		System.out.println("Hello Would");
+		for(int i = 30; i <= 1; i--){
+			System.out.println(i);
+		}
 	}
 }
